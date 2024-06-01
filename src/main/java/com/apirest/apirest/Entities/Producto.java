2d33d0a1f77f4cb8caa81c5aac9788bdb3c8c30e
Alex Alvarado <alexrichard.alvarado@gmail.com>
2024-06-01 17:40:03 +1200

    Crud API REST OK con Spring Boot

---
+++ src/main/java/com/apirest/apirest/Entities/Producto.java
@@ -0,0 +1,39 @@
+package com.apirest.apirest.Entities;
+
+// import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY) //Genera Id autoincrement
+    private Long id;
+    private String nombre;
+    private double precio;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    
+}
